a3b2452d3c811e302072d711f51b7c34d7ca9051
Tanner Green <tanner_green@nextlvlai.com>
2024-05-22 22:57:44 -0600

    got whole paragraph acquisition to work

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import io
+import re
 import pymupdf
 import fitz
 from routes.processPdf import extract_specifications_from_pdf
@@ -34,24 +35,31 @@ async def process(pdf_file: UploadFile = File(...)):
     try:
         # Save the uploaded file temporarily
         filename = pdf_file.filename
-        # contents = await pdf_file.read()
         file_content = await pdf_file.read()
     
     # Open the PDF from bytes using PyMuPDF
         pdf_document = pymupdf.open(stream=file_content, filetype="pdf")
-        
-        # Process the PDF as needed
-        # For example, extracting text from the first page
-        first_page = pdf_document[0]
-        text = first_page.get_text("text")
+        # first_page = pdf_document[0]
+        # text = first_page.get_text("text")
+        all_text = ""
+        for page_num in range(len(pdf_document)):
+            page = pdf_document[page_num]
+            all_text += page.get_text("text")
+        # regex_pattern = r'^.*warranty.*$'
+        # # return the data from the pdf
+        # matches = re.findall(regex_pattern, all_text, re.IGNORECASE | re.MULTILINE)
         # processed_text = extract_specifications_from_pdf(contents, regex_pattern)
+        regex_pattern = r'(\d+\.\d+.*?)(?=\n\d+\.\d+|\Z)'
+        
+        # Find all paragraphs
+        paragraphs = re.findall(regex_pattern, all_text, re.DOTALL)
+        
+        # Filter paragraphs containing the word "warranty"
+        warranty_paragraphs = [para for para in paragraphs if re.search(r'cummins', para, re.IGNORECASE)]
 
-        # Process the PDF using the separate function
+        print(warranty_paragraphs)
 
-        # Clean up the temporary file
-        os.remove(filename)
-
-        return {"result": text}
+        return {"result": warranty_paragraphs}
     except Exception as e:
         return {"error": str(e)}
 
