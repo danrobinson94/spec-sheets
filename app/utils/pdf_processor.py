32259b594039fffcd76c5d140e31d3499d2e86af
Tanner Green <tanner_green@nextlvlai.com>
2024-05-24 17:33:34 -0600

    Fixed new blocks method for pdf parser

--- app/utils/pdf_processor.py
+++ app/utils/pdf_processor.py
@@ -9,11 +9,9 @@ async def process_pdf(search_terms: list[str], pdf_file: UploadFile):
 
         pdf_document = pymupdf.open(stream=file_content, filetype="pdf")
         pdf_blocks = []
-        all_text = ""
         for page_num in range(len(pdf_document)):
             page = pdf_document[page_num]
             pdf_blocks.extend(page.get_text("blocks"))
-            all_text += page.get_text("text")
 
         output_list = []
 
@@ -27,23 +25,12 @@ async def process_pdf(search_terms: list[str], pdf_file: UploadFile):
             ):
                 if not any(keyword in text for keyword in ['STANDBY', 'GENERATOR', '432.07.100', '02/2024']):
                     output_list.append(text)
-
-        regex_pattern = r'(\d+\.\d+.*?)(?=\n\d+\.\d+|\Z)'
-        section_pattern = r'(\d+\.\d+.*?)(?=\n\d+\.\d+|\n[A-Z]\.|$)'
-        sub_section_pattern = r'([A-Z]\..*?)(?=\n[A-Z]\.|$)'
-        sections = re.findall(section_pattern, all_text, re.DOTALL)
-
-        sub_sections = []
-        for sub_section in sections:
-            sub_sections.append(re.findall(sub_section_pattern, sub_section, re.DOTALL))
-
-        paragraphs = re.findall(regex_pattern, all_text, re.DOTALL)
         
-        paragraphs2 = []
+        paragraphs = []
         for search_string in search_terms:
-            answer = [para for para in paragraphs if re.search(search_string, para, re.IGNORECASE)]
-            paragraphs2.append({search_string: answer})
+            answer = [para for para in output_list if re.search(search_string, para, re.IGNORECASE)]
+            paragraphs.append({search_string: answer})
 
-        return {"result": paragraphs2}
+        return {"result": paragraphs}
     except Exception as e:
         return {"error": str(e)}
