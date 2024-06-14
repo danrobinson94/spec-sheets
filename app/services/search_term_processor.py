00b7204c753e597e1b2076285e2746502ceced71
Tanner Green <tanner_green@nextlvlai.com>
2024-06-14 10:49:58 -0600

    pdf parser works great

--- app/services/search_term_processor.py
+++ app/services/search_term_processor.py
@@ -1,19 +1,5 @@
 import json
 from app.class_models import SearchTermInput
-import spacy
-
-# nlp = spacy.load("en_core_web_sm")
-
-# TODO: Use later
-def filter_keywords(text, search_term_input):
-    answers = []
-    for block in text:
-        doc = nlp(block[1])
-        for key in search_term_input.keywords:
-            subkeys = search_term_input.sub_search_terms
-            if key.lower() in doc.text.lower():
-                answers.append([sub_kw for sub_kw in subkeys if sub_kw in doc.text])
-    return []
 
 async def process_sub_search_terms(search_term: SearchTermInput, pdf_with_ref, depth, title):
     try:
@@ -41,14 +27,15 @@ async def process_search_terms(search_term: SearchTermInput, pdf_with_ref):
     try:
         pdf_blocks = []
         # Iterate through each array in the data
+        # [reference.copy(), line_coords, line_text, ref_depth, page_num]
         for index, array in enumerate(pdf_with_ref):
             # Check if the second element contains the search term
-            if any(term in array[1].lower() for term in search_term.keywords):
+            if any(term in array[2].lower() for term in search_term.keywords):
                 # Create a dictionary with 'header' and 'value' keys
                 term_dict = {
                     'title': search_term.title,
                     'reference': array[0],
-                    'value': array[1]
+                    'value': array[2]
                 }
                 # Append the dictionary to the search_term_array list
                 pdf_blocks.append(term_dict)
@@ -57,7 +44,7 @@ async def process_search_terms(search_term: SearchTermInput, pdf_with_ref):
                     # Iterate through sub search terms
                     for sub_search_term in search_term.subSearchTerms:
                         # Recursively call process_search_terms with sub search term
-                        sub_blocks = await process_sub_search_terms(sub_search_term, pdf_with_ref[index+1:], array[2], search_term.title)
+                        sub_blocks = await process_sub_search_terms(sub_search_term, pdf_with_ref[index+1:], array[3], search_term.title)
                         # Extend the pdf_blocks list with the results from sub search
                         pdf_blocks.extend(sub_blocks)
         if len(pdf_blocks) == 0:
--- app/utils/pdf_processor.py
+++ app/utils/pdf_processor.py
@@ -2,63 +2,141 @@ import re
 import pymupdf
 from fastapi import UploadFile
 
+def extract_text_with_coords(doc):
+    text_with_coords = []
+    
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        blocks = page.get_text("dict")["blocks"]
+        
+        for block in blocks:
+            if block['type'] == 0:  # Text block
+                for line in block["lines"]:
+                    span_texts = []
+                    for span in line["spans"]:
+                        span_texts.append(span["text"])
+                        x0, y0, x1, y1 = span["bbox"]
+                        text_with_coords.append((span["text"], page_num, (x0, y0, x1, y1)))
+                    
+    return text_with_coords
+
+def clean_text_with_coords(text_with_cords):
+    new_text_with_coords = []
+
+    i = 0
+    while i < len(text_with_cords) - 1:
+        ref = text_with_cords[i]
+        next_ref = text_with_cords[i + 1]
+        if abs(ref[2][1] - next_ref[2][1]) < 10:
+            new_coord = (ref[2][0], ref[2][1], next_ref[2][2], next_ref[2][3])
+            new_string = ref[0] + next_ref[0]
+            text_with_cords[i] = (new_string, ref[1], new_coord)
+            del text_with_cords[i+1]
+        else:
+            new_text_with_coords.append((ref[0].strip(), ref[1], ref[2]))
+            i += 1
+    return new_text_with_coords
+
+def combine_lines(pdf_layout):
+    final_layout = []
+
+    i = 0
+    ref = None
+    while i < len(pdf_layout) - 1:
+        ref = pdf_layout[i]
+        next_ref = pdf_layout[i + 1]
+        if ref[0] == next_ref[0]:
+            new_coord = (ref[1][0], ref[1][1], next_ref[1][2], next_ref[1][3])
+            new_string = ref[2] + "\n" + next_ref[2]
+            pdf_layout[i] = [ref[0], new_coord, new_string, next_ref[3], next_ref[4]]
+            del pdf_layout[i+1]
+        else:
+            final_layout.append(ref)
+            i += 1
+    final_layout.append(ref)
+    return final_layout
+
+def join_refs(pdf_layout):
+    for index, block in enumerate(pdf_layout):
+        ref_string = ""
+        ref_annot = ""
+        for ref in block[0]:
+            ref_string += ref[0] + " --> "
+            ref_annot += ref[0] + " --> " + ref[1] + " \n"
+        pdf_layout[index] = [ref_string[:-4].strip(), block[1], block[2].strip(), block[3], block[4], ref_annot.strip()]
+
+    return pdf_layout
+
+
 async def process_pdf(pdf_file: UploadFile):
     try:
+        part = r'PART\s+\d{1,2}(?:\s+|$)' # PART 1 2 3 etc...
+        section = r'SECTION\s+(?:[IVXLCDM]+)(?:\s+|$)' # SECTION I II III etc...
+        digit_dot_digit_header = r'\d{1,2}\.0{1,2}(?:\s+|$)' # 1.0 2.0 3.0 etc...
+        digit_dot_digit = r'\d{1,2}\.\d{1,2}(?:\s+|$)' # 1.1 1.2 2.1 2.2 etc...
+        digit_dot_digit_2 = r'\d{1,2}\.\d{1,2}\.\d{1,2}(?:\s+|$)' # 1.1.1 1.2.1 2.1.1 etc...
+        digit_dot_digit_3 = r'\d{1,2}\.\d{1,2}\.\d{1,2}\.\d{1,2}(?:\s+|$)' # 1.1.1.1 1.1.1.2 1.1.1.3 etc...
+        digit_dot = r'\d{1,2}\.(?:\s+|$)' # 1. 2. 3. etc...
+        digit_paren = r'\d{1,2}\)(?:\s+|$)' # 1) 2) 3) etc...
+        letter_u_dot = r'[A-Z]\.(?:\s+|$)' # A. B. C. etc...
+        letter_l_dot = r'[a-z]\.(?:\s+|$)' # a. b. c. etc...
+        letter_u_paren = r'[A-Z]\)(?:\s+|$)' # A) B) C) etc...
+        letter_l_paren = r'[a-z]\)(?:\s+|$)' # a) b) c) etc...
+        sections = [part, section, digit_dot_digit_header, digit_dot_digit, digit_dot_digit_2, digit_dot_digit_3, digit_dot,
+                 digit_paren, letter_u_dot, letter_l_dot, letter_u_paren, letter_l_paren]
+
         file_content = await pdf_file.read()
-        # search_terms = search_terms[0].split(',')
-
         pdf_document = pymupdf.open(stream=file_content, filetype="pdf")
-        text = ""
-        for page_num in range(len(pdf_document)):
-            page = pdf_document[page_num]
-            text += page.get_text("text")
-
-        lvl_1 = r'\nPART\s+\d+\s' # PART 1 2 3 etc... 
-        lvl_2 = r'\n\d+\.\d+\s+' # 1.1 1.2 2.1 2.2 etc...
-        lvl_3 = r'\n[A-Z]\.\s+' # A. B. C. etc...
-        lvl_4 = r'\n\d+\.\s+' # 1. 2. 3. etc...
-        lvl_5 = r'\n[a-z]\.\s+' # a. b. c. etc...
-        lvl_6 = r'\n\d+\)\s+' # 1) 2) 3) etc...
-        lvl_7 = r'\n[a-z]\)\s+' # a) b) c) etc...
-        lvls = [lvl_1, lvl_2, lvl_3, lvl_4, lvl_5, lvl_6, lvl_7]
-        pattern_match = r'|'.join(lvls)
-
         pdf_layout = []
         reference = []
+        ref_section = []
         ref_layout = {}
         ref_depth = 0
-        prev_match = ""
-        while text:
-            matches = re.findall(pattern_match, text)
-            if len(matches) != 0:
-                next_match = matches[0]
-                #TODO: Watch for when the lvls are skipped
-                #TODO: Still needs some fixes for typos and stuff
-                for lvl in lvls:
-                    if re.match(lvl, next_match):
-                        next_match_lvl = lvls.index(lvl)
-                        split_text = re.split(lvl, text, maxsplit=1)
-                        reference.append(prev_match)
-                        pdf_layout.append([reference.copy(), split_text[0], ref_depth])
-                        reference.pop()
-                        reference.append(prev_match + split_text[0])
-                        text = next_match.join(split_text[1:])
-                        ref_lvl = len(reference) - 1
-                        while ref_lvl >= 0:
-                            if next_match_lvl <= ref_lvl:
-                                reference.pop()
-                                ref_depth -= 1
-                            ref_lvl -= 1
-                        ref_depth += 1
-                        ref_layout[next_match] = ref_depth
-                        prev_match = next_match
-                        break
-            else:
-                pdf_layout.append([reference.copy(), text, ref_depth])
-                break
 
-        pdf_layout_with_ref = [[" ->\n".join([ref.strip() for ref in string[0]]), string[1], string[2]] for string in pdf_layout]
-        
-        return pdf_layout_with_ref
+        text_with_coords = extract_text_with_coords(pdf_document)
+        text_with_coords = clean_text_with_coords(text_with_coords)
+
+        for line in text_with_coords:
+            match_found = False
+            line_text = line[0]
+            page_num = line[1]
+            line_coords = line[2]
+            #TODO: Still needs some fixes for typos and stuff
+            for section in sections:
+                match = re.match(section, line_text)
+                if match:
+                    match_end = match.regs[0][1]
+                    if section not in ref_layout:
+                        ref_depth += 1
+                        reference.append([line_text[:match_end].strip(), line_text[match_end:]])
+                        ref_section.append(section)
+                        ref_layout[section] = ref_depth
+                    prev_ref = re.match(section, reference[len(reference)-1][0])
+                    if prev_ref:
+                        reference[len(reference)-1] = [line_text[:match_end].strip(), line_text[match_end:]]
+                        ref_section[len(ref_section)-1] = section
+                    else:
+                        while len(reference) > 0:
+                            prev_ref = re.match(section, reference[len(reference)-1][0])
+                            if prev_ref:
+                                reference[len(reference)-1] = [line_text[:match_end].strip(), line_text[match_end:]]
+                                ref_section[len(ref_section)-1] = section
+                                break
+                            ref_depth -= 1
+                            del ref_layout[ref_section[len(ref_section)-1]]
+                            reference.pop()
+                            ref_section.pop()
+                    split_text = re.split(section, line_text, maxsplit=1)
+                    pdf_layout.append([reference.copy(), line_coords, split_text[1], ref_depth, page_num])
+                    match_found = True
+                    break
+            if not match_found:
+                pdf_layout.append([reference.copy(), line_coords, line_text, ref_depth, page_num])
+
+        pdf_layout = combine_lines(pdf_layout)
+
+        pdf_layout = join_refs(pdf_layout)
+
+        return pdf_layout
     except Exception as e:
         return {"error": str(e)}
